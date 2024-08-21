0aa4e358167522d33acfb8a39d83a32b69c58576
stevy <stevyralambomanana@gmail.com>
2024-08-21 18:10:56 +0300

    build

---
+++ public/build/assets/SectionBorder--N2rMwz-.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as r,f as s,a as e}from"./app-mBY8x_ik.js";const c={},t={class:"hidden sm:block"},a=e("div",{class:"py-8"},[e("div",{class:"border-t border-gray-200 dark:border-gray-700"})],-1),d=[a];function n(_,i){return r(),s("div",t,d)}const m=o(c,[["render",n]]);export{m as S};
