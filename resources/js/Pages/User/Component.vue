c5a7d78986001698ca2b135dd27aabd5fc2ca48a
stevy-sys <stevyralambomanana@gmail.com>
2024-10-03 23:11:47 +0300

    chat

--- resources/js/Pages/User/Component.vue
+++ resources/js/Pages/User/Component.vue
@@ -13,9 +13,69 @@
 
 
         <!-- stevy -->
+        <div class="w-full">
+            <div class="flex">
+                <div class="w-[100px] h-[100px] relative rounded-full bg-red-500">
+                    <img class="w-[100px] h-[100px] rounded-full" src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" >
+                    <span class="w-[20px] border bg-green-500 h-[20px] right-[6px] bottom-[6px] absolute rounded-full"> </span>
+                </div>
+                <div class="w-[50%] pl-10">
+                    <h1 class="text-2xl">Chat with Khalid</h1>
+                    <span class="text-sm">4520 messages</span>
+                </div>
+            </div>
 
 
-        Lorem ipsum dolor sit amet.
+            <!-- ***************************************************************************************************** -->
+            <div class="mt-10 flex">
+                <div class="min-w-[2%]">
+                    <img class="w-[35px] h-[35px] rounded-full" src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" >
+                </div>
+                <div class="mx-5">
+                    <div class="  max-w-[50%] py-5 px-5 border rounded-tr-[20px] rounded-bl-[20px] min-h-[50px]" > Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis explicabo velit necessitatibus obcaecati omnis iste incidunt. Voluptatum, iure. Temporibus, repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, repudiandae vero architecto voluptatibus obcaecati veritatis nesciunt dolore nihil alias hic!</div>
+                    <span class="text-sm ml-5 text-white mt-3">20:45</span>
+                </div>
+            </div>
+
+            <div class="mt-10">
+                <div class="mx-5 justify-end flex">
+                    <div class="bg-red-400 max-w-[50%] py-5 px-5 border rounded-tl-[20px] rounded-br-[20px] min-h-[50px]" > Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis explicabo velit necessitatibus obcaecati omnis iste incidunt. Voluptatum, iure. Temporibus, repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, repudiandae vero architecto voluptatibus obcaecati veritatis nesciunt dolore nihil alias hic!</div>
+                </div>
+                <span class="justify-end mt-3 flex text-sm mr-5 text-white">20:45</span>
+            </div>
+
+            <!-- ***************************************************************************************************** -->
+
+
+
+
+            
+
+            <!-- ***************************************************************************************************** -->
+
+            <div class="mt-10 flex">
+                <div class="min-w-[2%]">
+                    <img class="w-[35px] h-[35px] rounded-full" src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" >
+                </div>
+                <div class="mx-5">
+                    <div class="  max-w-[50%] py-5 px-5 border rounded-tr-[20px] rounded-bl-[20px] min-h-[50px]" > Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis explicabo velit necessitatibus obcaecati omnis iste incidunt. Voluptatum, iure. Temporibus, repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, repudiandae vero architecto voluptatibus obcaecati veritatis nesciunt dolore nihil alias hic!</div>
+                    <img class="w-[25%] h-auto border rounded mt-3" src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" >
+                    <span class="text-sm ml-5 text-white mt-3">20:45</span>
+                </div>
+            </div>
+
+            <div class="mt-10">
+                <div class="mx-5 justify-end flex">
+                    <div class="bg-red-400 max-w-[50%] py-5 px-5 border border-red-400 rounded-tl-[20px] rounded-br-[20px] min-h-[50px]" > Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis explicabo velit necessitatibus obcaecati omnis iste incidunt. Voluptatum, iure. Temporibus, repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, repudiandae vero architecto voluptatibus obcaecati veritatis nesciunt dolore nihil alias hic!</div>
+                </div>
+                <div class="justify-end flex">
+                    <img class="w-[25%]  h-auto border border-red-400 rounded mt-3 mr-5" src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" >
+                </div>
+                <span class="justify-end mt-3 flex text-sm mr-5 text-white">20:45</span>
+            </div>
+
+            <!-- ***************************************************************************************************** -->
+        </div>
         <!-- fin stevy -->
     </div>
 </template>
