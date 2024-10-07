439acb5a9cc17a3e4b3e0846cf9c452621858934
stevy-sys <stevyralambomanana@gmail.com>
2024-10-07 21:35:30 +0300

    build

--- public/build/assets/Dashboard-ENWZjLX2.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./AdminLayout-CR1Y91Gi.js";import{o,c as t,w as e,a as r}from"./app-B63c1KuE.js";import"./ResponsiveNavLink-D8CtePQM.js";import"./iconify-BnITr9jR.js";const s=r("div",null," Dashboard ",-1),d={__name:"Dashboard",setup(_){return(c,n)=>(o(),t(a,null,{default:e(()=>[s]),_:1}))}};export{d as default};
